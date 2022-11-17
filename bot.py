758785d3deb4e5f70fd40335561cbfac42b445bf
Mahimai Raja J <mahimairaja3@gmail.com>
2022-11-17 19:42:50 +0530

    Updated version

--- bot.py
+++ bot.py
@@ -63,4 +63,4 @@ def run_discord_bot():
             await send_message(message, user_message, is_private=False)
             
         
-    client.run(os.getenv('TOKEN'))
+    client.run(os.environ['TOKEN'])
